22258ac7d28a12e8fc98139c7d4a7b7b99c31cac
Adam Gajdacs <gajdacsadam@inf.elte.hu>
2018-03-05 15:39:37 +0100

    Added user model, configured body parser, express session, flash messages and express validator

--- app.js
+++ app.js
@@ -15,17 +15,53 @@ const config = require('./config.js');
 mongoose.connect(config.database.mongodb);
 let db = mongoose.connection;
 db.once('open', function() {
-    console.log('Database connection established');
+    console.log('[MongoDB] Connection established');
 });
 db.on('error', function(err) {
-    console.log(err);
+    console.log('[MongoDB] ' + err);
 });
+
+// Models
+let User = require('./models/user');
 
 // Load View Engine
 var engine = require('express-dot-engine');
 app.engine('html', engine.__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Express Session
+app.use(session({
+    secret: 'The quick brown fox',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Flash Messages
+app.use(flash());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 
 // Home Route
 app.get('/', function(req, res) {
@@ -34,5 +70,5 @@ app.get('/', function(req, res) {
 
 // Start Server
 app.listen(3000, function() {
-    console.log('Listening on localhost:3000');
+    console.log('[Express] Listening on localhost:3000');
 });
