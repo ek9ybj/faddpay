c95bd4a0af9a3ea895ff7b08ad5e9c44ae02f4b7
Adam Gajdacs <gajdacsadam@inf.elte.hu>
2018-05-06 20:01:47 +0200

    Renamed the sample unit test

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const should = require('should');
 
 const server = supertest.agent('http://localhost:4000');
 
-describe('SAMPLE unit test', function () {
+describe('Guest home page test', function () {
     it('should return home page', function (done) {
         server.get('/').expect('Content-type', /text/).expect(200).end(function (err, res) {
             res.status.should.equal(200);
