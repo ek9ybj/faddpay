043c601e36b2649dada4cc70fcd3e68d700b2125
DDanii <ddani1995@gmail.com>
2018-05-20 19:51:31 +0200

    some tests added

--- test/test.js
+++ test/test.js
@@ -20,13 +20,7 @@ describe('Guest home page test', function () {
 });
 
 describe('Registration test', function () {
-    it('should register testuser1', function (done) {
-        server.post('/user/register').type('form').send({name: 'testuser1', email: 'testuser1@asd.com', password: 'testing', password2: 'testing'}).redirects(1).end(function (err, res) {
-            res.text.should.containEql('Successful registration!');
-            res.status.should.equal(200);
-            done();
-        });
-    });
+    
 });
 
 describe('Registration test', function () {
@@ -49,25 +43,109 @@ describe('Registration test', function () {
     });
 });
 
-describe('Login test', function () {
-    it('should login testuser1', function (done) {
+describe('Login and logout test', function () {
+    it('should login testuser2', function (done) {
         server.post('/user/login').type('form').send({email: 'testuser2@asd.com', password: 'testing'}).redirects(2).end(function (err, res) {
             res.text.should.containEql('Welcome back');
             res.status.should.equal(200);
             done();
         });
     });
-});
-
-describe('Logout test', function () {
-    it('should logout testuser1', function (done) {
-        server.get('/user/logout').redirects(1).end(function (err, res) {
+    it('should logout testuser2', function (done) {
+        server.get('/user/logout').redirects(2).end(function (err, res) {
             res.text.should.containEql('You\'ve logged out!');
             res.status.should.equal(200);
             done();
         });
     });
 });
+
+describe('deposit and send test', function () {
+    it('should register testuser3', function (done) {
+        server.post('/user/register').type('form').send({name: 'testuser3', email: 'testuser3@asd.com', password: 'testing', password2: 'testing'}).redirects(1).end(function (err, res) {
+            res.text.should.containEql('Successful registration!');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should login testuser2', function (done) {
+        server.post('/user/login').type('form').send({email: 'testuser2@asd.com', password: 'testing'}).redirects(2).end(function (err, res) {
+            res.text.should.containEql('Welcome back');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should deposit to testuser2', function (done) {
+        server.post('/account/deposit').type('form').send({amount: '100', currency: 'HUF'}).redirects(1).end(function (err, res) {
+            res.text.should.containEql('has been deposited');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should send money to testuser3', function (done) {
+        server.post('/payment/send').type('form').send({email: 'testuser3@asd.com', currency: 'HUF', amount: '10'}).redirects(1).end(function (err, res) {
+            res.text.should.containEql('has been sent');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should display history to testuser2', function (done) {
+        server.get('/account/history').end(function (err, res) {
+            res.text.should.containEql('10');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should display balance to testuser2', function (done) {
+        server.get('/').redirects(1).end(function (err, res) {
+            res.text.should.containEql('90');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should logout testuser2', function (done) {
+        server.get('/user/logout').redirects(2).end(function (err, res) {
+            res.text.should.containEql('You\'ve logged out!');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+});
+
+describe('Request test', function () {
+    
+    it('should register testuser1', function (done) {
+        server.post('/user/register').type('form').send({name: 'testuser1', email: 'testuser1@asd.com', password: 'testing', password2: 'testing'}).redirects(1).end(function (err, res) {
+            res.text.should.containEql('Successful registration!');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should login testuser2', function (done) {
+        server.post('/user/login').type('form').send({email: 'testuser2@asd.com', password: 'testing'}).redirects(2).end(function (err, res) {
+            res.text.should.containEql('Welcome back');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should request from testuser1', function (done) {
+        server.post('/payment/request').type('form').send({email: 'testuser1@asd.com', currency: 'HUF', amount: '10'}).redirects(1).end(function (err, res) {
+            res.text.should.containEql('has been requested');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+    it('should logout testuser2', function (done) {
+        server.get('/user/logout').redirects(2).end(function (err, res) {
+            res.text.should.containEql('You\'ve logged out!');
+            res.status.should.equal(200);
+            done();
+        });
+    });
+});
+
+
+
 
 //add more tests here
 
@@ -81,6 +159,7 @@ async function removeTestUsers() {
     let db = mongoose.connection;
     await User.findOneAndRemove({ name: 'testuser1' });
     await User.findOneAndRemove({ name: 'testuser2' });
+    await User.findOneAndRemove({ name: 'testuser3' });
     db.close();
     console.log('Test users have been removed.');
 }
